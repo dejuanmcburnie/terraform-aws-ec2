625d6e372d9a258069b60edab7e5cc32cd2e75cb
dejuanmcburnie <96082416+dejuanmcburnie@users.noreply.github.com>
2026-05-05 15:30:36 -0400

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,10 +1,10 @@
 variable "region" {}
-variable "ec2instancename" {}
-variable "amiid" {}
-variable "vpcid" {}
-variable "subnetid" {}
-variable "instancetype" {}
-variable "securitygroupid" {}
-variable "keyname" {}
+variable "ec2_instance_name" {}
+variable "ami_id" {}
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "instance_type" {}
+variable "security_group_id" {}
+variable "key_name" {}
 variable "tags" {type = map(string) }
 variable "remotestatekey" {}
