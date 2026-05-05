76d250e4b660d2af0b8e6388c9fe78fb157a3cbf
dejuanmcburnie <96082416+dejuanmcburnie@users.noreply.github.com>
2026-05-05 15:45:02 -0400

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,16 +1,10 @@
 resource "aws_instance" "this" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+  ami           = "ami-0b83e5863cd4520e4"
+  instance_type = "t3.micro"
+  subnet_id     = "subnet-0c14806b04c3bfae1"
 
-  vpc_security_group_ids = var.security_group_ids
+  vpc_security_group_ids = "sg-082b43b860166451d"
 
-  key_name = var.key_name
-
-  tags = merge(
-    {
-      Name = var.name
-    },
-    var.tags
+  key_name = "CuroDevKey"
   )
 }
